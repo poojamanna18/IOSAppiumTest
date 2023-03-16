7cfed2002812b94ba5fa73f9071ba06d8603e9a0
pooja.manna <poojamanna16@gmail.com>
2023-03-16 14:55:23 +0530

    Updated the dependencies and added the app file in project level

--- src/test/java/IOSTest.java
+++ src/test/java/IOSTest.java
@@ -2,16 +2,19 @@
 import io.appium.java_client.ios.IOSDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.Assert;
+
+
 
 public class IOSTest {
     private IOSDriver driver;
-    @Before
+    @BeforeEach
     public void setUp() throws MalformedURLException {
 //Setting Desired Capabilities
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -19,13 +22,13 @@ public class IOSTest {
         capabilities.setCapability("appium:platformVersion", "16.2");
         capabilities.setCapability("appium:deviceName", "iPhone 14");
         capabilities.setCapability("appium:udid", "8128696F-0EA1-41B3-A2BC-3FBB97F2871C");
-        capabilities.setCapability("appium:app", "/Users/pooja.manna/Downloads/Other_iOS_apps/HelloWorldiOS.app");
+        capabilities.setCapability("appium:app", System.getProperty("user.dir")+ "/src/test/resources/apps/HelloWorldiOS.app");
         capabilities.setCapability("appium:automationName", "XCUITest");
         driver = new IOSDriver(new URL("http://localhost:4723/wd/hub"), capabilities);
         System.out.println("------Driver initialized-------"+driver);
     }
 
-    @After
+    @AfterEach
     public void tearDown() {
         if (driver != null) {
             driver.quit();
@@ -35,7 +38,7 @@ public class IOSTest {
     @Test
     public void testFindingAnElement() throws InterruptedException {
         driver.findElement(By.xpath("//XCUIElementTypeStaticText[@name='Click me!']")).click();
-        Assert.assertEquals(driver.findElement(By.xpath("//XCUIElementTypeStaticText[@name=\"You successfully clicked the button!\"]")).getText(),"You successfully clicked the button!");
+       Assertions.assertEquals(driver.findElement(By.xpath("//XCUIElementTypeStaticText[@name=\"You successfully clicked the button!\"]")).getText(),"You successfully clicked the button!");
     }
 }
 
